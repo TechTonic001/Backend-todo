ceb6f140e8ad27fe2046e5717bab974a45396c53
TechTonic <amoohalleluyah1@gmail.com>
2026-03-14 12:25:44 +0100

    change

--- router/user.route.js
+++ router/user.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const User = require("../Models/User"); 
+const User = require("../models/User"); 
 const jwt = require("jsonwebtoken");
 
 const router = express.Router();
